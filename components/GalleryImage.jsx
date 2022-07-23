6e1564bb1343dbf865cdeb7c95dcf27081f41182
brandonn <brandon7.7porcel@gmail.com>
2022-07-23 18:59:05 -0300

    cambie img a Image(en local no funca, supuestamente en produccion sí

--- components/GalleryImage.jsx
+++ components/GalleryImage.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import Image from 'next/image';
 export default function GalleryImage({ title, author, poster }) {
 	return (
 		<>
-			<img
+			<Image
 				loading="lazy"
 				src={poster}
 				title={title}
 				alt={`${title}, ${author}`}
-			></img>
+			></Image>
 		</>
 	);
 }
